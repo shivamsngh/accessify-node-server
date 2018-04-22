54a615a8bcbc76abcce43ededc093cdf6bfca4b7
Singh, Shivam D <shivam.d.singh@accenture.com>
2018-04-22 13:18:50 +0200

    added tensor output string manip program

--- analysis-service/image-description.service.js
+++ analysis-service/image-description.service.js
@@ -14,13 +14,17 @@ class DescriptionService {
             let scpt = exec(`sh ../shellscripts/tensrscpt.sh ${image}`, (error, stdout, stderr) => {
                 console.log(stdout);
                 console.log(stderr);
-                resolve(stdout);
+                resolve(this.extractHighlyProbableOutput(stdout));
                 if (error !== null) {
                     console.log(`exec error: ${error}`);
                     reject(error);
                 }
             });
         });
+    }
+
+    extractHighlyProbableOutput(stringInput) {
+        return stringInput.match(/0\).*\(p/)[0].replace('0)', '');
     }
 }
 
